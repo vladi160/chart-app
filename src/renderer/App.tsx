520d509309482079bd35d176dd70d68bca1bcfd9
vladi160 <vskazd@yahoo.com>
2022-08-18 16:12:47 +0300

    first commit 4

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,65 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import Chart from './Chart';
-import icon from '../../assets/icon.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 const Hello = () => {
+
+
+  const [data, setData] = useState(null);
+  const [fileErr, setFileErr] = useState(null);
+
+  const onFileChange = (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = e => {
+      try {
+        const json = JSON.parse(e.target.result);
+        setData(json);
+        fileErr(null);
+      } catch (err) {
+        setFileErr('Invalid file!');
+        console.log(err);
+      }
+    };
+    reader.readAsText(e.target.files[0])
+  }
+
+  const renderChart = () => {
+
+    if (data) {
+      return (
+        <Chart data={data} />
+      );
+    } else if (fileErr) {
+
+      return (
+        <h2 className='hErr'>{fileErr}</h2>
+      )
+
+    }
+
+    return (
+      <h2 className='hMsg'>No file selected!</h2>
+    )
+  }
+
+  const onClear = (e) => {
+    e.preventDefault();
+    setData(null);
+  }
+
+  console.log('data')
+  console.log(data)
+
   return (
     <div>
-      <Chart />
+      <form>
+        <input type="file" accept=".json" onChange={onFileChange} />
+        {(data !== null) && <button onClick={onClear}>Clear</button>}
+      </form>
+      {renderChart()}
     </div>
   );
 };
--- src/renderer/Chart.js
+++ src/renderer/Chart.js
@@ -1,9 +1,45 @@
-
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import style from './Chart.mpodule.js';
+
 
 const Chart = ({ data }) => {
 
+    data = (data && data.length) ? data.map(el => { return { y: parseFloat(el.y), name: el.label } }) : [];
+    const renderChart = () => {
+
+        if (data && data.length) {
+
+            return (
+                <BarChart
+                    layout="vertical"
+                    width={1600}
+                    height={data.length * 60}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="y" type='number' />
+                    <YAxis type="category" width={450} padding={{ left: 20 }} dataKey="name" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="y" fill="#8884d8" />
+                </BarChart>
+            )
+        }
+
+        return <></>;
+    }
+
     return (
-        <></>
+        <>
+            {renderChart()}
+        </>
     )
 }
+
+export default Chart;
